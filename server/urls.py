9160ad27797f8e96a3d7f67d097c91b449bc725a
Sun Liwen <sunliwen@gmail.com>
2013-11-14 23:51:23 +0800

    Add users, sessions module.

--- server/urls.py
+++ server/urls.py
@@ -1,12 +1,24 @@
 from django.conf.urls import patterns, include, url
+from rest_framework.routers import DefaultRouter
+
+from sessions.views import SessionView
+from users.views import UserViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+
+    # Authentication
+    url(r'^session/$', SessionView.as_view()),
+
+    # API
+    url(r'api/', include(router.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 )
