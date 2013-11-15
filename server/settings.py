4430299b6c49598477e67cad3fc7e68abac198e7
Sun Liwen <sunliwen@gmail.com>
2013-11-15 18:46:27 +0800

    Add testing user liwen:1234

--- server/settings.py
+++ server/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'compressor',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -53,6 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
@@ -71,6 +74,7 @@ DATABASES = {
     }
 }
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -85,7 +89,30 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Compressor
+
+NODE_ROOT = os.path.join(BASE_DIR, 'node_modules')
+HANDLEBARS_PATH = os.path.join(NODE_ROOT, 'django-ember-precompile', 'bin', 'django-ember-precompile')
+COMPRESS_PRECOMPILERS = (
+    ('text/x-handlebars', '{} {{infile}}'.format(HANDLEBARS_PATH)),
+)
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+
+# Django REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
--- server/urls.py
+++ server/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
-from session.views import SessionView
+from sessions.views import SessionView
 from users.views import UserViewSet
 
 
